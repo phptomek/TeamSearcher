fbac44c869b68503d3e709dd3dfad17c0484e9a9
Mateuszto <mateusz@mateuszt.pl>
2019-08-13 22:12:51 +0200

    Little static data changes.

--- post/views.py
+++ post/views.py
@@ -6,14 +6,14 @@ posts = [
         'author': 'Player4',
         'game': 'Dota 2',
         'content': 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
-        'link': 'steamcommunity.com/user=Player4',
+        'link': 'https://steamcommunity.com/profiles/76561198325967622',
         'date_posted': 'August 14, 2019'
     },
     {
         'author': 'Player7',
         'game': 'Rocket League',
         'content': 'Looking for mate to play 2s',
-        'link': 'steamcommunity.com/user=Player7',
+        'link': 'https://steamcommunity.com/profiles/76561198325967622',
         'date_posted': 'August 18, 2019'
     },
 ]
