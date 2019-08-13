9d76be332f76ef81a820c93fcbd594611a9af4bf
Mateuszto <mateusz@mateuszt.pl>
2019-08-13 22:04:56 +0200

    Start with templates.

--- post/views.py
+++ post/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Player4',
+        'game': 'Dota 2',
+        'content': 'Im looking for team - 2 people',
+        'link': 'steamcommunity.com/user=Player4',
+        'date_posted': 'August 14, 2019'
+    },
+    {
+        'author': 'Player7',
+        'game': 'Rocket League',
+        'content': 'Looking for mate to play 2s',
+        'link': 'steamcommunity.com/user=Player7',
+        'date_posted': 'August 18, 2019'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'post/home.html', context)
 
